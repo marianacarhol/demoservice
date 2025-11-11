efd54475c2c981f9d9a6bafbdd7215d747d15a9e
marianacarhol <marianacarrilloholguin@outlook.com>
2025-11-11 15:37:24 -0700

    added deletePost in routes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     createPost,
     getAllPosts,
+    deletePost
     
 } from '../controllers/postController.js'
 
@@ -9,6 +10,6 @@ const router = express.Router();
 
 router.post('/', createPost);
 router.get('/', getAllPosts);
-router.delete('/:id', getAllPosts);
+router.delete('/:id', deletePost);
 
 export default router;
